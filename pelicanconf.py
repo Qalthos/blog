94c8dace90931da0d310272fd704ea16c71a5a00
Nathaniel Case <Qalthos@gmail.com>
2013-04-05 21:55:23 -0400

    Initial commit with gitignore

---
+++ pelicanconf.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u'Nathaniel Case'
+SITENAME = u'Why Not Wingnut?'
+SITEURL = 'http://blog.linkybook.com'
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG = u'en'
+
+# Blogroll
+LINKS =  (('FOSS@RIT', 'http://foss.rit.edu'),
+         )
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = False
